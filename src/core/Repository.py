5497c73df630b4fc3b83291dc5242b475655141c
Lars-Erik <lea@markedspartner.no>
2023-09-19 23:40:37 +0200

    UOW & Repo implementations inherit empty base classes for sugar (interface lookalikes)

--- src/core/Repository.py
+++ src/core/Repository.py
@@ -1,5 +1,14 @@
 class Repository:
-    def create(self, entity): pass
+    """
+    Implementations handle the storage and retrieval of entities
+    """
+    def create(self, entity):
+        """
+        Persists a new entity. Make sure to provide a unique key.
+        :param entity: The new entity
+        :return: None
+        """
+        pass
     def update(self, entity): pass
     def delete(self, entity): pass
     def read(self, id): return None
